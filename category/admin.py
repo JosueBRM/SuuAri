f0ab056ac210d76227be5e803e801e52197d83f6
JosueBRM <josuederker2022@gmail.com>
2023-10-07 14:05:44 -0600

    Now Homepage is render with data dynamically
    A lot of stuff changed from last commit, help me :cry:

--- category/admin.py
+++ category/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Category
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('category_name',)}
+    list_display = ('category_name', 'slug')
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
