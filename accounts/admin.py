bbac0c957153f08494e2edb09f974b69186accee
JosueBRM <josuederker2022@gmail.com>
2023-10-07 11:54:28 -0600

    Password field is now uneditable

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
 
