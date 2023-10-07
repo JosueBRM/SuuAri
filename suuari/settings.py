997e960f02f883bb7bbdd2353529f82ab8a73886
JosueBRM <josuederker2022@gmail.com>
2023-10-07 12:51:11 -0600

    Media files setup

--- suuari/settings.py
+++ suuari/settings.py
@@ -125,6 +125,10 @@ STATICFILES_DIRS = [
     'suuari/static',
 ]
 
+#Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
