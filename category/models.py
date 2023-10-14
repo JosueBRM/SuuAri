378f48d1505be3ad1e9bf39590dc7ca2011c9c3c
JosueBRM <josuederker2022@gmail.com>
2023-10-13 18:15:45 -0600

    Product detail view added (dummy))

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
@@ -10,7 +11,12 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
+    def get_url(self):
+            return reverse('products_by_category', args=[self.slug])
+
     def __str__(self):
         return self.category_name
+    
+    
 
 
--- store/views.py
+++ store/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from store.models import Product
+from category.models import Category
+
 
 # Create your views here.
-def store(request):
-    return render(request, 'store/store.html')
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
+    context = {
+        'products': products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    return render(request, 'store/product_detail.html')
