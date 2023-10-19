9104b39e3d071b7f6526280959e3685433531b86
JosueBRM <josuederker2022@gmail.com>
2023-10-18 18:30:40 -0600

    Pass context to cart view

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,55 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from store.models import Product
+from .models import Cart, CartItem
 
 # Create your views here.
 
-def cart(request):
-    return render(request, 'store/cart.html')
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+def add_cart(request, product_id):
+    product = Product.objects.get(id=product_id) #get the product
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request)) #get the cart using the cart id present on session
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1 #increases the cart quantity
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product = product,
+            quantity = 1,
+            cart = cart,
+        )
+        cart_item.save()
+
+    return redirect('cart')
+
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except ObjectNotExist:
+        pass 
+
+    context = {
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }
+
+    return render(request, 'store/cart.html', context)
+
