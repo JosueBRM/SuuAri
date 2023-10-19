be3c6dd356249ca6858d6c553b2f3c37bc83d1a0
JosueBRM <josuederker2022@gmail.com>
2023-10-18 18:51:56 -0600

    Cart view with dynamic data :monkey_face:

--- carts/models.py
+++ carts/models.py
@@ -16,6 +16,11 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
+    
+
+    def sub_total(self):
+        return self.product.price * self.quantity
+        
 
     def __str__(self):
         return self.product
