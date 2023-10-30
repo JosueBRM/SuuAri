64b8dab43a8bf6cedab421d917dc8492d72d54a4
JosueBRM <josuederker2022@gmail.com>
2023-10-30 13:51:35 -0600

    Added to cart feature with view button

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from store.models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
 
 
 # Create your views here.
@@ -26,10 +29,13 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists() #check cart model
+        
     except Exception as e:
         raise e
     
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
