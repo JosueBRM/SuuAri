560cda42bce887426c78df658e08ec7143f5a269
JosueBRM <josuederker2022@gmail.com>
2023-11-11 12:06:57 -0600

    POST method on register

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,5 +17,9 @@ class RegistrationForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your email here'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your phone number'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
