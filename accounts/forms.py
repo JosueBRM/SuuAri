1b940e4a5b84741dc261066edc57573654eb1ac9
JosueBRM <josuederker2022@gmail.com>
2023-11-11 13:59:33 -0600

    Password and email validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django import forms
 from .models import Account
 
@@ -15,6 +16,16 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password: 
+            raise forms.ValidationError(
+                "Password does not match!"
+            )
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your first name'
@@ -23,3 +34,5 @@ class RegistrationForm(forms.ModelForm):
         self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your phone number'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+    
